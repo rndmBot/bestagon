508a83758d350b9c197b1c12016319090eedf42d
Aleksandr Antonov <antonov.o779@gmail.com>
2025-10-18 23:36:41 +0300

    v0.3.0

--- bestagon/domain/application.py
+++ bestagon/domain/application.py
@@ -1,36 +1,51 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 from typing import List, Tuple
 
 from bestagon.core.event_store import EventStore
 from bestagon.core.mapper import Mapper
-from bestagon.core.policy import ApplicationName, ProjectionName
+from bestagon.core.system import EventSourcedSystem
 from bestagon.domain.domain_event import DomainEvent
 
 
-class Application:
-    def __init__(self, name: ApplicationName, event_store: EventStore, mapper: Mapper):
+class EventProcessor(ABC):
+    def __init__(self):
+        self._system = None
+
+    @property
+    def name(self) -> str:
+        return self.get_name()
+
+    @property
+    def system(self) -> EventSourcedSystem:
+        return self._system
+
+    @abstractmethod
+    def get_name(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def process_event(self, event: DomainEvent) -> None:
+        raise NotImplementedError
+
+    def set_system(self, system: EventSourcedSystem) -> None:
+        if not isinstance(system, EventSourcedSystem):
+            raise TypeError(f'Expected <{EventSourcedSystem.__class__.__qualname__}>, got {type(system)}')
+        self._system = system
+
+
+class Leader(EventProcessor):
+    pass
+
+
+class Follower(EventProcessor):
+    def __init__(self):
         super().__init__()
-        self._event_store = event_store
-        self._mapper = mapper
-        self._name = name
-        self._leaders: List[ApplicationName] = list()
+        self._leaders: List[str] = list()
 
     @property
-    def name(self) -> ApplicationName:
-        return self._name
-
-    @property
-    def event_store(self) -> EventStore:
-        return self._event_store
-
-    @property
-    def leaders(self) -> Tuple[ApplicationName, ...]:
+    def leaders(self) -> Tuple[str, ...]:
         return tuple(self._leaders)
-
-    @property
-    def mapper(self) -> Mapper:
-        return self._mapper
 
     def follow(self, application: 'Application') -> None:
         if not isinstance(application, Application):
@@ -40,9 +55,25 @@ class Application:
 
         self._leaders.append(application.name)
 
+
+class Application(Leader, Follower):
+    def __init__(self, event_store: EventStore, mapper: Mapper):
+        super().__init__()
+        self._event_store = event_store
+        self._mapper = mapper
+
+    @property
+    def event_store(self) -> EventStore:
+        return self._event_store
+
+    @property
+    def mapper(self) -> Mapper:
+        return self._mapper
+
     def get_events(self, start_position: int, limit: int) -> List[Tuple[DomainEvent, int]]:
         """Returns two-tuple (DomainEvent, CommitPosition)"""
         # TODO - rethink return type
+        # TODO - ACHTUNG - should be refactored
         stream_events = self.event_store.get_events(regex_list=[self.name.to_regex()], start_position=start_position, limit=limit)
 
         domain_events = list()
@@ -53,41 +84,12 @@ class Application:
 
         return domain_events
 
-    @abstractmethod
-    def process_event(self, event: DomainEvent) -> None:
-        raise NotImplementedError
 
-
-class Projection:
+class Projection(Follower):
     # TODO - Projections should give a possibility to provide stats about how it is doing
     # TODO - Projection stats should be accessed through REST API http://localhost/projectionStats/CountByDocument
-
-    def __init__(self, name: ProjectionName):
-        super().__init__()
-        self._name = name
-        self._leaders: List[ApplicationName] = list()
-
-    @property
-    def leaders(self) -> Tuple[ApplicationName, ...]:
-        return tuple(self._leaders)
-
-    @property
-    def name(self) -> ProjectionName:
-        return self._name
 
     @abstractmethod
     def drop(self) -> None:
         """There should be functionality to drop projection and rebuild it from scratch"""
-        raise NotImplementedError
-
-    def follow(self, application: 'Application') -> None:
-        if not isinstance(application, Application):
-            raise TypeError
-        if application.name in self.leaders:
-            return
-
-        self._leaders.append(application.name)
-
-    @abstractmethod
-    def process_event(self, event: DomainEvent) -> None:
         raise NotImplementedError
